634870ae9dcbceca3cfade9a0df24cfe9acb5088
YouRanXY <xyoung393@gmail.com>
2026-07-03 20:17:33 +0800

    second commit

--- test.cpp
+++ test.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;
     return 0;
 }
